8eddd8e63ecaf4c10dd365edf165ffd312792b70
henricomaeda <henrico.maeda@gmail.com>
2023-07-31 17:50:18 -0300

    Updated main window setup

--- desktop/app/windows/main_window.py
+++ desktop/app/windows/main_window.py
@@ -5,14 +5,23 @@ from customtkinter import CTk
 class MainWindow(CTk, WindowBaseUtils):
     def __init__(self, dark_mode: bool = False, **resources) -> None:
         super().__init__(**resources)
-        self.toggle_appearance(dark_mode)
+        self.dark_mode = not dark_mode
         self.setup_window()
-
-    def toggle_appearance(self, dark_mode: bool = None):
-        self.dark_mode = dark_mode if dark_mode is not None else not self.dark_mode
-        appearance = "dark" if self.dark_mode else "light"
-        self._set_appearance_mode(appearance)
 
     def setup_window(self):
         self.title("uRemotePC")
         self.center_window(width=800, height=600)
+        self.toggle_appearance()
+        self.create_widgets()
+        self.display_widgets()
+
+    def toggle_appearance(self):
+        self.dark_mode = not self.dark_mode
+        appearance = "dark" if self.dark_mode else "light"
+        self._set_appearance_mode(appearance)
+
+    def create_widgets(self):
+        pass
+
+    def display_widgets(self):
+        pass
