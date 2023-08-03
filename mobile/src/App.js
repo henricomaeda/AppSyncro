c194b9c0a2bd76fbdf24c8863e535ff86d227f2c
henricomaeda <henrico.maeda@gmail.com>
2023-08-03 13:11:04 -0300

    Removed header from screens

--- mobile/src/App.js
+++ mobile/src/App.js
@@ -1,7 +1,5 @@
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Icon from "react-native-vector-icons/MaterialIcons";
-import { TouchableOpacity } from "react-native";
 import * as React from "react";
 
 import FormScreen from "./screens/FormScreen";
@@ -21,21 +19,8 @@ const App = () => {
     return (
         <NavigationContainer theme={customTheme}>
             <Stack.Navigator
-                initialRouteName="MainScreen"
-                screenOptions={({ navigation, route }) => ({
-                    headerTitle: globals.appName,
-                    headerLeft: () => route.name === "MainScreen" && (
-                        <TouchableOpacity
-                            style={{ marginRight: globals.window.width / 22 }}
-                            onPress={null}>
-                            <Icon
-                                color={globals.colors.placeholder}
-                                size={globals.window.width / 16.2}
-                                name="arrow-back"
-                            />
-                        </TouchableOpacity>
-                    )
-                })}>
+                initialRouteName="FormScreen"
+                screenOptions={{ headerTitle: globals.appName, headerShown: false }}>
                 <Stack.Screen name="FormScreen" component={FormScreen} />
                 <Stack.Screen name="MainScreen" component={MainScreen} />
             </Stack.Navigator>
