d79e87e4e82d091fa7a51186fa787fa9344e7688
henricomaeda <henrico.maeda@gmail.com>
2023-08-04 15:40:48 -0300

    Added simple styles

--- mobile/src/screens/MainScreen/MainScreenStyles.js
+++ mobile/src/screens/MainScreen/MainScreenStyles.js
@@ -2,8 +2,19 @@ import { StyleSheet } from "react-native";
 import { globals } from "../../Globals";
 
 const styles = StyleSheet.create({
-    "": {
-
+    "container": {
+        padding: globals.window.width / 12,
+        justifyContent: "center",
+        flexGrow: 1
+    },
+    "titleContainer": {
+        flexDirection: "row",
+        alignSelf: "center"
+    },
+    "title": {
+        fontSize: globals.window.width / 22,
+        color: globals.colors.tint,
+        fontWeight: "bold"
     }
 });
 
