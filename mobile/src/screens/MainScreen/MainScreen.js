839d1abdae27665cd9d4f07ff3512d384bd307da
henricomaeda <henrico.maeda@gmail.com>
2023-08-05 01:56:05 -0300

    Added more widgets and commands

--- mobile/src/screens/MainScreen/MainScreen.js
+++ mobile/src/screens/MainScreen/MainScreen.js
@@ -1,4 +1,6 @@
 import React, { useRef, useState, useEffect } from "react";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import LinearGradient from "react-native-linear-gradient";
 import TcpSocket from "react-native-tcp-socket";
 import {
     ActivityIndicator,
@@ -6,7 +8,8 @@ import {
     PanResponder,
     ScrollView,
     View,
-    Text
+    Text,
+    TextInput
 } from "react-native";
 import { globals } from "../../Globals";
 import styles from "./MainScreenStyles";
@@ -17,7 +20,8 @@ const MainScreen = ({ navigation, route }) => {
         serverPort: 12345,
         serverPassword: "",
     });
-    const [response, setResponse] = useState("");
+    const [response, setResponse] = useState("Development");
+    const [keyboard, setKeyboard] = useState("");
     const submit = useRef(console.log);
 
     useEffect(() => {
@@ -53,7 +57,6 @@ const MainScreen = ({ navigation, route }) => {
         else disconnect();
     }, []);
 
-
     const [isPressed, setIsPressed] = useState(false);
     const intervalRef = useRef(null);
     const panResponder = useRef(
@@ -65,10 +68,10 @@ const MainScreen = ({ navigation, route }) => {
                 intervalRef.current = setInterval(() => {
                     const moveX = gestureState.moveX !== 0 ? gestureState.moveX : gestureState.x0;
                     const moveY = gestureState.moveY !== 0 ? gestureState.moveY : gestureState.y0;
-                    const dx = Math.max(Math.min(Math.round((moveX - gestureState.x0) / 2), 20), -20);
-                    const dy = Math.max(Math.min(Math.round((moveY - gestureState.y0) / 2), 20), -20);
-                    submit.current(`${dx}MM${dy}`);
-                }, 60);
+                    const dx = Math.max(Math.min((moveX - gestureState.x0), 120), -120);
+                    const dy = Math.max(Math.min((moveY - gestureState.y0), 120), -120);
+                    submit.current(`${Math.round(dx / 6)} MM ${Math.round(dy / 6)}`);
+                }, 100);
             },
             onPanResponderRelease: () => {
                 clearInterval(intervalRef.current);
@@ -78,61 +81,179 @@ const MainScreen = ({ navigation, route }) => {
     ).current;
 
     if (response) return (
-        <ScrollView contentContainerStyle={styles.container}>
-            <View style={styles.titleContainer}>
-                <Text style={styles.title}>Connected with </Text>
-                <Text
-                    style={[
-                        styles.title,
-                        { color: globals.colors.primary }
-                    ]}>
-                    {serverIp}:{"***" + serverPort.toString().slice(3)}
-                </Text>
-            </View>
-            <View
-                {...panResponder.panHandlers}
-                style={{
-                    backgroundColor: isPressed ? globals.colors.midground : globals.colors.foreground,
-                    borderTopRightRadius: globals.window.width / 20,
-                    borderTopLeftRadius: globals.window.width / 20,
-                    marginBottom: globals.window.width / 42,
-                    marginTop: globals.window.width / 20,
-                    height: globals.window.width / 2
-                }}
+        <View style={{ flex: 1 }}>
+            <LinearGradient
+                colors={[globals.colors.primary, globals.colors.secondary]}
+                start={{ x: 0, y: 0 }}
+                end={{ x: 1, y: 1 }}
+                pointerEvents="none"
+                style={styles.backgroundCircle}
             />
-            <View style={{ justifyContent: "space-between", flexDirection: "row" }}>
-                <TouchableOpacity
-                    onPress={() => submit.current("LC")}
-                    onLongPress={() => submit.current("HLC")}
-                    style={{
-                        borderBottomLeftRadius: globals.window.width / 20,
-                        backgroundColor: globals.colors.foreground,
-                        height: globals.window.width / 10,
-                        flex: 1
-                    }}
+            <ScrollView
+                keyboardDismissMode="on-drag"
+                keyboardShouldPersistTaps={true}
+                contentContainerStyle={styles.container}>
+                <View style={styles.titleContainer}>
+                    <Text style={styles.title}>Connected with </Text>
+                    <Text
+                        style={[
+                            styles.title,
+                            { color: globals.colors.primary }
+                        ]}>
+                        {serverIp}:{"***" + serverPort.toString().slice(3)}
+                    </Text>
+                </View>
+                <TextInput
+                    value={keyboard}
+                    keyboardAppearance="dark"
+                    onChangeText={setKeyboard}
+                    placeholder="Enter something to write..."
+                    placeholderTextColor={globals.colors.placeholder}
+                    selectionColor={globals.colors.placeholder}
+                    style={styles.keyboard}
+                    maxLength={1000}
                 />
-                <TouchableOpacity
-                    onPress={() => submit.current("MC")}
-                    onLongPress={() => submit.current("HMC")}
+                <View style={styles.keyboardButtons}>
+                    <TouchableOpacity
+                        onPress={() => {
+                            const text = keyboard.trim();
+                            if (text.length > 0) {
+                                setKeyboard("");
+                                submit.current(`WRITE ${text}`);
+                            };
+                        }}
+                        style={[
+                            styles.keyboardButton,
+                            {
+                                borderBottomLeftRadius: globals.window.width / 26,
+                                backgroundColor: globals.colors.secondary,
+                            }
+                        ]}>
+                        <Icon
+                            name="draw"
+                            color={globals.colors.tint}
+                            size={globals.window.width / 20}
+                            style={{ marginRight: 1 }}
+                        />
+                        <Text style={styles.keyboardText}>
+                            Write it
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => submit.current("BACKSPACE")}>
+                        <LinearGradient
+                            colors={[globals.colors.secondary, globals.colors.primary]}
+                            start={{ x: 0, y: 0 }}
+                            end={{ x: 1, y: 0 }}
+                            style={styles.keyboardButton}>
+                            <Icon
+                                name="backspace"
+                                color={globals.colors.tint}
+                                size={globals.window.width / 20}
+                                style={{ marginRight: 2 }}
+                            />
+                            <Text style={styles.keyboardText}>
+                                Backspace
+                            </Text>
+                        </LinearGradient>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => submit.current("ENTER")}
+                        style={[
+                            styles.keyboardButton,
+                            {
+                                borderBottomRightRadius: globals.window.width / 26,
+                                backgroundColor: globals.colors.primary
+                            }
+                        ]}>
+                        <Icon
+                            name="keyboard-return"
+                            color={globals.colors.tint}
+                            size={globals.window.width / 20}
+                            style={{ marginTop: 2 }}
+                        />
+                        <Text style={styles.keyboardText}>
+                            Enter
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+                <View
+                    {...panResponder.panHandlers}
                     style={{
-                        marginHorizontal: globals.window.width / 42,
-                        backgroundColor: globals.colors.foreground,
-                        height: globals.window.width / 10,
-                        flex: 1
-                    }}
-                />
-                <TouchableOpacity
-                    onPress={() => submit.current("RC")}
-                    onLongPress={() => submit.current("HRC")}
-                    style={{
-                        borderBottomRightRadius: globals.window.width / 20,
-                        backgroundColor: globals.colors.foreground,
-                        height: globals.window.width / 10,
-                        flex: 1
-                    }}
-                />
-            </View>
-        </ScrollView>
+                        backgroundColor: isPressed ? globals.colors.midground : globals.colors.foreground,
+                        borderTopRightRadius: globals.window.width / 20,
+                        borderTopLeftRadius: globals.window.width / 20,
+                        marginBottom: globals.window.width / 42,
+                        height: globals.window.width / 2,
+                        justifyContent: "center",
+                        alignItems: "center"
+                    }}><Text style={styles.label}>Hold to mouse movement</Text></View>
+                <View style={{ justifyContent: "space-between", flexDirection: "row" }}>
+                    <TouchableOpacity
+                        onPress={() => submit.current("LC")}
+                        onLongPress={() => submit.current("HLC")}
+                        style={{
+                            borderBottomLeftRadius: globals.window.width / 20,
+                            backgroundColor: globals.colors.foreground,
+                            justifyContent: "center",
+                            alignItems: "center",
+                            flex: 1
+                        }}>
+                        <Text style={styles.label}>
+                            Left-click
+                        </Text>
+                    </TouchableOpacity>
+                    <View
+                        style={{
+                            marginHorizontal: globals.window.width / 42,
+                            height: globals.window.width / 6,
+                            flex: 1
+                        }}>
+                        <TouchableOpacity
+                            onPress={() => submit.current("SU")}
+                            style={{
+                                backgroundColor: globals.colors.midground,
+                                flex: 1
+                            }}
+                        />
+                        <TouchableOpacity
+                            onPress={() => submit.current("MC")}
+                            onLongPress={() => submit.current("HMC")}
+                            style={{
+                                backgroundColor: globals.colors.foreground,
+                                marginVertical: globals.window.width / 90,
+                                justifyContent: "center",
+                                alignItems: "center",
+                                flex: 2
+                            }}>
+                            <Text style={styles.label}>
+                                Mid-click
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity
+                            onPress={() => submit.current("SD")}
+                            style={{
+                                backgroundColor: globals.colors.midground,
+                                flex: 1
+                            }}
+                        />
+                    </View>
+                    <TouchableOpacity
+                        onPress={() => submit.current("RC")}
+                        onLongPress={() => submit.current("HRC")}
+                        style={{
+                            borderBottomRightRadius: globals.window.width / 20,
+                            backgroundColor: globals.colors.foreground,
+                            justifyContent: "center",
+                            alignItems: "center",
+                            flex: 1
+                        }}>
+                        <Text style={styles.label}>
+                            Right-click
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+        </View>
     )
     else return (
         <ActivityIndicator
