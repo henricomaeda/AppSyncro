1f7b5746f145e4f22d892aeef70cd2ed242d8475
henricomaeda <henrico.maeda@gmail.com>
2023-08-08 14:09:35 -0300

    Fixed volume issue

--- desktop/utils/process_system.py
+++ desktop/utils/process_system.py
@@ -1,43 +1,41 @@
 from win32security import AdjustTokenPrivileges, LookupPrivilegeValue, OpenProcessToken, TOKEN_ADJUST_PRIVILEGES, SE_PRIVILEGE_ENABLED, SE_SHUTDOWN_NAME, TOKEN_QUERY
+from comtypes import CoUninitialize, CoInitialize, CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 from win32api import GetCurrentProcess, SetSystemPowerState
 from ctypes import windll, cast, POINTER
-from comtypes import CLSCTX_ALL
 
 
 @staticmethod
 def get_volume():
+    CoInitialize()
     devices = AudioUtilities.GetSpeakers()
     interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
     volume = cast(interface, POINTER(IAudioEndpointVolume))
     current_volume_db = volume.GetMasterVolumeLevel()
+    CoUninitialize()
     return volume, current_volume_db
 
 
 @staticmethod
-def set_volume(volume, volume_db):
+def set_volume(volume: IAudioEndpointVolume, volume_db: float):
     volume_db = max(min(volume_db, 0), -60)
     volume.SetMasterVolumeLevel(volume_db, None)
 
 
-@staticmethod
 def increase_volume():
     volume, current_volume_db = get_volume()
     set_volume(volume, current_volume_db + 2.0)
 
 
-@staticmethod
 def decrease_volume():
     volume, current_volume_db = get_volume()
     set_volume(volume, current_volume_db - 2.0)
 
 
-@staticmethod
 def lock_screen():
     windll.user32.LockWorkStation()
 
 
-@staticmethod
 def hibernate_system():
     priv_flags = (TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY)
     hToken = OpenProcessToken(GetCurrentProcess(), priv_flags)
