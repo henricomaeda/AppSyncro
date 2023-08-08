67c73a0824877964bd64eb7a0204bb82781c78b7
henricomaeda <henrico.maeda@gmail.com>
2023-08-08 10:45:17 -0300

    Created the key button styles

---
+++ mobile/src/components/KeyButton/KeyButtonStyles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+import { globals } from "../../Globals";
+
+const styles = StyleSheet.create({
+    "keyButtonImage": {
+        marginHorizontal: globals.window.width / 46,
+        marginVertical: globals.window.width / 40,
+        height: globals.window.width / 8,
+        width: globals.window.width / 8
+    }
+});
+
+export default styles;
